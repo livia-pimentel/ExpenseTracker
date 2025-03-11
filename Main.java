00e500ea6676f3035e88cdbfc9164c002cdaef74
Livia Pimentel <livia_macedo89@yahoo.com.br>
2025-03-11 18:20:42 -0300

    Initial Main class

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Scanner; 
+
+/*
+ * class Main that starts the ExpenseTracker application
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in); // Read the user input
+        scanner.useLocale(Locale.US); // Sets the scanner to use US locale
+        ExpenseTracker tracker = new ExpenseTracker();
+
+        // Collects the user’s information
+        System.out.print("Enter the expense category: ");
+        String category =  scanner.nextLine();
+
+        System.out.print("Enter the expense description: ");
+        String description = scanner.nextLine();
+
+        System.out.print("Enter the amount of the expense: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine(); // Clear the buffer
+
+        Expense expense = new Expense(LocalDate.now(), description, amount, category);
+
+        tracker.addExpense(expense);
+
+        String report = tracker.generateReport();
+        System.out.println(report);
+
+        scanner.close();
+
+    }
+}
